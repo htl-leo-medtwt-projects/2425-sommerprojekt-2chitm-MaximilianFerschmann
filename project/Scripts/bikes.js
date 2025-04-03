d70d15032e086e6bca185e7507df2729dc6517d5
Maximilian Ferschmann <m.ferschmann@students.htl-leonding.ac.at>
2025-04-03 09:00:26 +0200

    Dynamische content generierung in Bikes.html

--- project/Scripts/bikes.js
+++ project/Scripts/bikes.js
@@ -1,3 +1,28 @@
-function loadBikes(){
-    
+const bikesContainer = document.getElementById('bikesContainer');
+
+let bikes = [];
+fetch('../Data/bikes.json')
+  .then(response => response.json())
+  .then(data => {
+    bikes = data;
+    console.log(bikes);
+    loadAllBikes();
+  })
+  .catch(error => console.error('Fehler:', error));
+
+function loadAllBikes(){
+        bikes.forEach(bike => {
+            bikesContainer.innerHTML += `
+                <div class="bike-item">
+                    <img src="../${bike.image}" alt="${bike.name}">
+                    <h3>${bike.model}</h3>
+                    <p>${bike.stroke}</p>
+                    <p>Price: ${bike.price}€</p>
+                    <p>Horsepower: ${bike.hp}</p>
+                    <p>Weight: ${bike.weight}</p>
+                    <button class="like">Like</button>
+                </div>
+            `;
+                     
+        });
 }
