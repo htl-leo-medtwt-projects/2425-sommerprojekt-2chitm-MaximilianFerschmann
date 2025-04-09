d1453dbec48b85cc93c7d1979fd024df9510985d
Maximilian Ferschmann <m.ferschmann@students.htl-leonding.ac.at>
2025-04-09 11:48:10 +0200

    Refactor bike loading functionality, update filter options, and adjust CSS for better layout

--- project/Scripts/bikes.js
+++ project/Scripts/bikes.js
@@ -7,22 +7,11 @@ fetch('../Data/bikes.json')
   .then(data => {
     bikes = data;
     console.log(bikes);
-    loadAllBikes();
+    loadBikes(bikes);
   })
   .catch(error => console.error('Fehler:', error));
 
-function loadAllBikes(){
-        bikes.forEach(bike => {
-            bikesContainer.innerHTML += `
-                <div class="bike-item">
-                    <img src="../${bike.image}" alt="${bike.model}">
-                    <h3>${bike.model}</h3>
-                    <button onclick="loadBikeDetails('${bike.model}')"> Show Details </button>
-                </div>
-            `;
-                     
-        });
-}
+
 const detailContainer = document.getElementById('bikeDetails');
 function loadBikeDetails(bikename) {
   detailContainer.className = 'flex';
@@ -45,13 +34,13 @@ function loadBikeDetails(bikename) {
         <p id="bikePower">PS: ${bike.hp}</p>    
         </div>
     `
-  
+
   } else {
     console.log('Bike not found');
   }
 
 }
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     closeDetails();
   }
@@ -60,4 +49,45 @@ function closeDetails() {
   detailContainer.className = 'hidden';
   bikesContainer.style.opacity = '1';
   document.getElementById('filter').style.opacity = '1';
+}
+
+function loadFilteredBikes() {
+  let name = document.getElementById('filter-input').value;
+  let strokes = document.getElementById('strokes').value;
+  let maxccm = parseInt(document.getElementById('maxccm').value) || Infinity;
+  let minccm = parseInt(document.getElementById('minccm').value) || 0;
+  let maxprice = parseInt(document.getElementById('maxprice').value) || Infinity;
+  let minprice = parseInt(document.getElementById('minprice').value) || 0;
+  let model = document.getElementById('bike-model').value;
+
+  // Filterlogik
+  let filteredBikes = bikes.filter(bike => {
+    return (
+      bike.model.toLowerCase().includes(name.toLowerCase()) &&
+      (bike.stroke == strokes || strokes === 'all') &&
+      (bike.cc <= maxccm) &&
+      (bike.cc >= minccm) &&
+      (bike.price <= maxprice) &&
+      (bike.price >= minprice) &&
+      (bike.type.toLowerCase().includes(model.toLowerCase()) || model === 'all')
+    );
+  });
+
+  // Container leeren und gefilterte Bikes lade
+  bikesContainer.innerHTML = ''; 
+  loadBikes(filteredBikes);
+}
+
+function loadBikes(bikes){
+  console.log(bikes)
+  bikes.forEach(bike => {
+    bikesContainer.innerHTML += `
+                <div class="bike-item">
+                    <img src="../${bike.image}" alt="${bike.model}">
+                    <h3>${bike.model}</h3>
+                    <button onclick="loadBikeDetails('${bike.model}')"> Show Details </button>
+                </div>
+            `;
+
+  });
 }
